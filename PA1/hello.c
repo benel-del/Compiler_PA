0c4d5bc4556b08934ed1c78ea2a83803fa4016bb
benel-del <52874329+benel-del@users.noreply.github.com>
2021-03-20 18:25:24 +0900

    Create hello.c

---
+++ PA1/hello.c
@@ -0,0 +1,9 @@
+// just sample for tokenizer.l
+
+int main(void){
+        float _b2;
+        _b2 = 40.12359E+2;
+        _b2 *= (0.23 + (001024 % '\t'));
+        printf("Hello\tWorld!\n");
+        return 0;
+}
